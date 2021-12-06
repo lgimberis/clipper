ddf783016dcd71e37d9f7a75ed63d3e014c6a477
lgimberis <lukasgimberis@gmail.com>
2021-12-06 18:28:42 +0000

    Remove debug line. Add error message if no file is dragged on. Ensure all streams are copied.

--- clipper/clipper.py
+++ clipper/clipper.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 import time
 import tkinter as tk
+from tkinter import messagebox
 from tkinter import ttk
 
 import ffmpy
@@ -50,50 +51,50 @@ def seconds_to_timestamp_builder(maximum):
 def run_clip(file_in, file_out, start, duration):
     ff = ffmpy.FFmpeg(
         inputs={str(file_in): ['-ss', start]},
-        outputs={str(file_out): ['-c', 'copy', '-t', duration]},
+        outputs={str(file_out): ['-c', 'copy', '-map', '0', '-t', duration]},
     )
-    ff.run()
+    ff.run(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
-def main(file=None):
+def main():
     master = tk.Tk()
+    master.title('Clipper')
     if len(sys.argv) > 1:
         file = Path(sys.argv[1])
+
+        if file.is_file():
+            ff = ffmpy.FFprobe(
+                global_options=('-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1'),
+                inputs={file: None},
+            )
+            output = ff.run(stdout=subprocess.PIPE, stderr=subprocess.PIPE,)
+            file_duration = int(float(output[0].decode(locale.getpreferredencoding()).strip()))
+
+            main_frame = ttk.Frame(master)
+            main_frame.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+
+            w = RangeSlider(main_frame, value_min=0, value_max=file_duration, value_display=seconds_to_timestamp_builder(file_duration))
+            w.grid(row=1)
+
+            def clip():
+                b["state"] = "disabled"
+                b["text"] = "Running..."
+                # Grab a timestamp for unique filename
+                file_out = file.with_stem(f"{file.stem}_clip{time.strftime('%H%M%S')}")
+                value_in, value_out = w.get_in_and_out()
+                timestamp_ss = seconds_to_timestamp(value_in)
+                timestamp_t = seconds_to_timestamp(value_out - value_in)
+                master.update()
+                run_clip(file, file_out, timestamp_ss, timestamp_t)
+                master.quit()
+
+            b = ttk.Button(main_frame, text="Clip", command=clip)
+            b.grid(row=2, sticky=(tk.E))
+
+            tk.mainloop()
     else:
-        if not file:
-            return
-
-    if file.is_file():
-        ff = ffmpy.FFprobe(
-            global_options=('-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1'),
-            inputs={file: None},
-        )
-        output = ff.run(stdout=subprocess.PIPE)
-        file_duration = int(float(output[0].decode(locale.getpreferredencoding()).strip()))
-
-        main_frame = ttk.Frame(master)
-        main_frame.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
-
-        w = RangeSlider(main_frame, value_min=0, value_max=file_duration, value_display=seconds_to_timestamp_builder(file_duration))
-        w.grid(row=1)
-
-        def clip():
-            b["state"] = "disabled"
-            b["text"] = "Running..."
-            # Grab a timestamp for unique filename
-            file_out = file.with_stem(f"{file.stem}_clip{time.strftime('%H%M%S')}")
-            value_in, value_out = w.get_in_and_out()
-            timestamp_ss = seconds_to_timestamp(value_in)
-            timestamp_t = seconds_to_timestamp(value_out - value_in)
-            master.update()
-            run_clip(file, file_out, timestamp_ss, timestamp_t)
-            master.quit()
-
-        b = ttk.Button(main_frame, text="Clip", command=clip)
-        b.grid(row=2, sticky=(tk.E))
-
-        tk.mainloop()
+        messagebox.showerror("Error", "Please drag a file onto Clipper.")
 
 
 if __name__ == "__main__":
-    main(Path('1.mp4'))
+    main()
